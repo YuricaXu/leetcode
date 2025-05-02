30eccc591ce3984b93813c8aee5c12f184eb7a30
YuricaXu <fx2@fordham.edu>
2025-05-02 16:09:09 -0400

    Time: 1 ms (97.85%), Space: 19.4 MB (40.5%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def findErrorNums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        s = n*(n+1)//2
+        miss = s - sum(set(nums))
+        duplicate = sum(nums) + miss - s
+        return [duplicate, miss]
