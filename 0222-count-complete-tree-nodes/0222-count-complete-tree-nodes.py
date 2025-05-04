964a1d247a19524d709c07244b7bd8d21bbb0e92
YuricaXu <fx2@fordham.edu>
2025-05-04 00:46:47 -0400

    Time: 3 ms (57.71%), Space: 23.4 MB (9.4%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        return self.getNodesNum(root)
+        
+    def getNodesNum(self, cur):
+        if not cur:
+            return 0
+        leftNum = self.getNodesNum(cur.left) #左
+        rightNum = self.getNodesNum(cur.right) #右
+        treeNum = leftNum + rightNum + 1 #中
+        return treeNum     
