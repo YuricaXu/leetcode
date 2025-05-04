4d1c51f8a52293f8d9903eee0f7fb8a5321765e4
YuricaXu <fx2@fordham.edu>
2025-05-04 00:49:58 -0400

    Time: 57 ms (67.03%), Space: 21.1 MB (48.24%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def traversal(self, cur, p, q):
+        if cur is None:
+            return cur
+                                                        # 中
+        if cur.val > p.val and cur.val > q.val:           # 左
+            left = self.traversal(cur.left, p, q)
+            if left is not None:
+                return left
+
+        if cur.val < p.val and cur.val < q.val:           # 右
+            right = self.traversal(cur.right, p, q)
+            if right is not None:
+                return right
+
+        return cur
+
+    def lowestCommonAncestor(self, root, p, q):
+        return self.traversal(root, p, q)
