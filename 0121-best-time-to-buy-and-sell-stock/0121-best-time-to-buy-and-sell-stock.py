44e28589ff0e12a0586902a900086052c64df0e2
YuricaXu <fx2@fordham.edu>
2025-05-03 22:56:42 -0400

    Time: 143 ms (12.17%), Space: 27 MB (9.04%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        low = float("inf")
+        result = 0
+        for i in range(len(prices)):
+            low = min(low, prices[i]) #取最左最小价格
+            result = max(result, prices[i] - low) #直接取最大区间利润
+        return result        
