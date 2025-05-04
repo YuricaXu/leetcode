96f48647d538842824e87ca2844a874a0ca7b4f9
YuricaXu <fx2@fordham.edu>
2025-05-03 22:58:27 -0400

    Time: 49 ms (60.25%), Space: 32.8 MB (85.63%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        self.backtracking(s, 0, [], result)
+        return result
+
+    def backtracking(self, s, start_index, path, result ):
+        # Base Case
+        if start_index == len(s):
+            result.append(path[:])
+            return
+        
+        # 单层递归逻辑
+        for i in range(start_index, len(s)):
+            # 若反序和正序相同，意味着这是回文串
+            if s[start_index: i + 1] == s[start_index: i + 1][::-1]:
+                path.append(s[start_index:i+1])
+                self.backtracking(s, i+1, path, result)   # 递归纵向遍历：从下一处进行切割，判断其余是否仍为回文串
+                path.pop()             # 回溯
+        
